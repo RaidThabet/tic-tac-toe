0b69e5257c7a35ac6cf9ccdd75641c3f42d05dec
Raid Thabet <thabet.raid123@gmail.com>
2024-07-11 19:09:45 +0100

    Adjust UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { useState } from "react";
-import Header from "../components/Header";
-import Game from "../components/Game";
+import Header from "./components/Header";
+import Game from "./components/Game";
 import "./App.css";
 
 function App() {
